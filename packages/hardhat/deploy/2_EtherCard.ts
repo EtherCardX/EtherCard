e01a4540901885827e80857612ced7320c1f1b19
Kenk <kenghin_lim@hotmail.com>
2023-03-19 10:24:18 +0900

    feat: add smart contracts

---
+++ packages/hardhat/deploy/2_EtherCard.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+import { ethers } from 'hardhat';
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }: any) => {
+  const { deploy, read, execute } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  // Chain Dependent Settings
+  if (chainId == '25') {
+  } else if (chainId == '338') {
+  } else if (chainId == '31337') {
+  } else {
+    return;
+  }
+
+  // Get Entry Point Contract
+  const entryPoint = await deployments.get('EntryPoint');
+
+  let contract = await deploy('EtherCard', {
+    from: deployer,
+    log: true,
+    args: [entryPoint.address],
+  });
+};
+
+module.exports.tags = ['EtherCard'];
